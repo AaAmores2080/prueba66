cf56d467407d265b3b76fe37266b7ef399e1c2d2
AaAmores2080 <AaAmores2080@angiezuluaga@gmail.com>
2021-07-08 17:48:03 -0500

    se crea clase producto con metodos y accesorios y su respectivo objeto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, a todos");
+        System.out.println("La tienda de Ropa");
+
+        Producto productoUno = new Producto();
+
+        productoUno.setNombre("Falda");
+        productoUno.setPrecio(70000);
+        productoUno.mostrar();
+
     }
 }
